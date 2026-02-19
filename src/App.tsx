2731f3ed4302845cee689694238f6df6cd89835d
AF-AI-io <uw.01.2023@gmail.com>
2026-02-19 18:24:59 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/automation-architect">  {/* ← Add this line – critical for subpath */}
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
